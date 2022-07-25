3b49f466c91e07f08f779a2eed93ea1b9b669066
Derrick Hammer <derrick@derrickhammer.com>
2022-07-25 02:47:35 -0400

    *Add build step to build all native modules then copy the library to its build/Release folder *Ensure we don't load a loady instance as a sub-module

--- prebuild.sh
+++ prebuild.sh
@@ -10,3 +10,12 @@ rimraf node_modules/@tendermint/belt/node_modules/@tendermint/types
 rimraf node_modules/@pokt-network/pocket-js/dist/web.js
 rimraf node_modules/@pokt-network/amino-js/dist/web*
 rimraf node_modules/supports-color
+rimraf node_modules/*/node_modules/loady
+
+for pkg in bcrypto udx-native secp256k1 bigint-buffer bufferutil sodium-native bdb goosig mrmr utf-8-validate; do
+    (
+        cd "node_modules/${pkg}" || return
+        prebuildify -t "$(node -v)"
+        cp "build/Release/obj.target/"*.node "build/Release/"
+    )
+done
