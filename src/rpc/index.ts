383ec64692bd0d8e7444de559c0f0a9fcb570a63
Derrick Hammer <derrick@derrickhammer.com>
2022-08-04 23:45:09 -0400

    *Update rpc method list

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -7,6 +7,8 @@ import { default as EvmMethods } from "./evm.js";
 import { default as HnsMethods } from "./handshake.js";
 import { default as SolMethods } from "./solana.js";
 import { default as AlgoMethods } from "./algorand.js";
+import { default as IpfsMethods } from "./ipfs.js";
+import { default as MiscMethods } from "./misc.js";
 
 export const rpcMethods: RpcMethodList = Object.assign(
   {},
@@ -14,5 +16,7 @@ export const rpcMethods: RpcMethodList = Object.assign(
   EvmMethods,
   HnsMethods,
   SolMethods,
-  AlgoMethods
+  AlgoMethods,
+  IpfsMethods,
+  MiscMethods
 );
