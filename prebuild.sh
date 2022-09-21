7df0a5cf09ac707347c432d0cc463139fbadbe89
Derrick Hammer <derrick@derrickhammer.com>
2022-09-21 07:15:39 -0400

    *clean up build script since most code has been ported to plugins

--- prebuild.sh
+++ prebuild.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-rimraf node_modules/@tendermint/types
-rimraf node_modules/@pokt-network/amino-js/node_modules/@tendermint/belt/types
-rimraf node_modules/@pokt-network/amino-js/node_modules/@tendermint/belt/src
-rimraf node_modules/@pokt-network/amino-js/node_modules/@tendermint/belt/index.ts
-rimraf node_modules/@pokt-network/amino-js/node_modules/@tendermint/belt/dist/web*
-rimraf node_modules/@tendermint/belt/types
-rimraf node_modules/@tendermint/belt/node_modules/@tendermint/types
-rimraf node_modules/@pokt-network/pocket-js/dist/web.js
-rimraf node_modules/@pokt-network/amino-js/dist/web*
-rimraf node_modules/supports-color
-rimraf node_modules/*/node_modules/loady
+rimraf node_modules/libskynetnode/node_modules/node-fetch
 
-for pkg in bcrypto udx-native secp256k1 bigint-buffer bufferutil sodium-native bdb goosig mrmr utf-8-validate; do
+for pkg in bcrypto udx-native sodium-native; do
     (
         cd "node_modules/${pkg}" || return
         prebuildify -t "$(node -v)"
