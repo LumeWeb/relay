58e95806d006e0c58df1b6a3e0f4162337dc2b42
Derrick Hammer <derrick@derrickhammer.com>
2023-03-29 16:23:56 -0400

    *hook on core.pluginsLoaded to ensure that we don't answer until all plugins are loaded

--- src/plugins/core.ts
+++ src/plugins/core.ts
@@ -1,8 +1,17 @@
 import { Plugin, PluginAPI } from "@lumeweb/relay-types";
 
+let pluginsLoadedResolve: () => void;
+let pluginsLoadedPromise = new Promise<void>((resolve) => {
+  pluginsLoadedResolve = resolve;
+});
+
 const plugin: Plugin = {
   name: "core",
   async plugin(api: PluginAPI): Promise<void> {
+    api.once("core.pluginsLoaded", () => {
+      pluginsLoadedResolve();
+    });
+
     api.registerMethod("ping", {
       cacheable: false,
       async handler(): Promise<any> {
@@ -13,6 +22,10 @@ const plugin: Plugin = {
     api.registerMethod("get_methods", {
       cacheable: false,
       async handler(): Promise<any> {
+        await pluginsLoadedPromise;
+
+        console.log("get_methods", api.rpcServer.getMethods());
+
         return api.rpcServer.getMethods();
       },
     });
