47f768b20b2f412d9171ba4e6fea049684c9d744
Derrick Hammer <derrick@derrickhammer.com>
2023-04-23 09:10:31 -0400

    ci: need sudo on go install, chmod it, then access it directly

--- ci/publish.sh
+++ ci/publish.sh
@@ -4,9 +4,11 @@ if ! command -v go &>/dev/null; then
     sudo apt-get update && sudo apt-get install -y golang
 fi
 
-go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
+sudo go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
+sudo chmod +x /root/go/bin/nfpm
 
 yq -i ".version=\"${1}\"" nfpm.yaml
-nfpm package -p deb
+sudo /root/go/bin/nfpm package -p deb
+
 pip install --upgrade cloudsmith-cli
 cloudsmith push deb lumeweb/lume-web-relay *.deb
