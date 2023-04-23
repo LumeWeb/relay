c08e130e97b6f96132d1054504952848d8991420
Derrick Hammer <derrick@derrickhammer.com>
2023-04-23 09:02:44 -0400

    ci: ci already has yq

--- ci/publish.sh
+++ ci/publish.sh
@@ -5,7 +5,6 @@ if ! command -v go &>/dev/null; then
 fi
 
 go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
-go install github.com/mikefarah/yq/yq@latest
 
 yq -i ".version=\"${1}\"" nfpm.yaml
 nfpm package -p deb
