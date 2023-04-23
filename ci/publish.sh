ec43f49562acbb18e4fb8f063f8b0abc79e9348e
Derrick Hammer <derrick@derrickhammer.com>
2023-04-23 08:46:12 -0400

    ci: update publish script to install golang if needed

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+if ! command -v go &>/dev/null; then
+    sudo apt-get update && sudo apt-get install -y golang
+fi
+
 go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
 go install github.com/mikefarah/yq/yq@latest
 
